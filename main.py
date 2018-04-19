1ee60ba161e73c1b6e8503cc5ab8b0cb8a321b5a
NimmiW <nimmirashinika@gmail.com>
2018-04-19 13:06:54 +0530

    - initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from show_results import plot_results
+
+year = 2012
+from_month = 2
+to_month = 5
+currency = "EURUSD"
+
+plot_results(year, from_month, to_month, currency)
---
+++ show_results.py
@@ -0,0 +1,49 @@
+import datetime
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy
+
+
+def plot_results(year, from_month, to_month, currency):
+
+    start_date = str(year) + "-" + str(from_month) + "-01"
+    #end_date = request.form["year"] + "-" + str(int(request.form["to_month"]) + 1) + "-01"
+    if (to_month == 12):
+        end_date = str(to_month + 1) + "-" + str(1) + "-01"
+    else:
+        end_date = str(year) + "-" + str(to_month + 1) + "-01"
+
+    quotes = pd.read_csv("C:/Users/HP/PycharmProjects/plot_results/data/"+currency+"/DAT_MT_"+currency+"_M1_" + str(year) + ".csv")
+
+
+    quotes['Time'] = quotes[['Date', 'Time']].apply(lambda x: ' '.join(x), axis=1)
+    quotes['Time'] = quotes['Time'].apply(lambda x: pd.to_datetime(x) - datetime.timedelta(hours=2))
+    quotes.index = quotes.Time
+
+    print(quotes)
+    # select desired range of dates
+    mask = (quotes.index > start_date) & (quotes.index <= end_date)
+    quotes = quotes.loc[mask]
+
+    print(quotes)
+
+    fig, ax = plt.subplots()
+    ax.plot(quotes['Close'])
+    ax.set_title('Black Regions')
+    anormalies = pd.read_csv("results/EURUSD/all_anomalies.csv")
+
+    anormalies['Time'] = anormalies['DateHour'].apply(lambda x: pd.to_datetime(x))
+    anormalies.index = anormalies.Time
+    mask = (anormalies.index > start_date) & (anormalies.index <= end_date)
+    anormalies = anormalies.loc[mask]
+    #xdate = [datetime.datetime.fromtimestamp(i) for i in quotes['Time']]
+
+    for index, row in anormalies.iterrows():
+        a_index = row["Time"]
+        b_index = row["Time"]+datetime.timedelta(hours=1)
+        ax.axvspan(a_index, b_index, color='red', alpha=0.5)
+
+    plt.show()
+
+
+#https://stackoverflow.com/questions/42373104/candlestick-ochl-graph
