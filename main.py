9001f937a43898395e081ea5ebfd88fb339d5131
NimmiW <nimmirashinika@gmail.com>
2018-05-22 12:59:42 +0530

    - minor changes

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from show_results import plot_results
-
+"""
 year = 2012
 from_month = 2
 to_month = 5
 currency = "EURUSD"
+"""
 
-x = input("input")
-print(x)
+print("Please Enter the Range of months")
+year = int(input("Year : "))
+from_month = int(input("From Month : "))
+to_month = int(input("To Month : "))
+currency = "EURUSD"
+print("Currency : EURUSD (by default)")
+print("Generating the graph...")
+#currency = input("Currency : EURUSD / GBPUSD ?")
 
 plot_results(year, from_month, to_month, currency)
--- show_results.py
+++ show_results.py
@@ -9,33 +9,40 @@ def plot_results(year, from_month, to_month, currency):
     start_date = str(year) + "-" + str(from_month) + "-01"
     #end_date = request.form["year"] + "-" + str(int(request.form["to_month"]) + 1) + "-01"
     if (to_month == 12):
-        end_date = str(to_month + 1) + "-" + str(1) + "-01"
+        end_date = str(year + 1) + "-" + str(1) + "-01"
     else:
         end_date = str(year) + "-" + str(to_month + 1) + "-01"
 
-    quotes = pd.read_csv("C:/Users/HP/PycharmProjects/plot_results/data/"+currency+"/DAT_MT_"+currency+"_M1_" + str(year) + ".csv")
+    quotes = pd.read_csv("data/"+currency+"/DAT_MT_"+currency+"_M1_" + str(year) + ".csv")
+    print(quotes)
+    # select desired range of dates
+    quotes['Date_x'] = quotes['Date'].apply(lambda x: pd.to_datetime(x) - datetime.timedelta(hours=2))
+    quotes.index = quotes.Date_x
 
+    mask_1 = (quotes.index >= start_date) & (quotes.index <= end_date)
+    quotes = quotes.loc[mask_1]
+
+    print(quotes)
 
     quotes['Time'] = quotes[['Date', 'Time']].apply(lambda x: ' '.join(x), axis=1)
     quotes['Time'] = quotes['Time'].apply(lambda x: pd.to_datetime(x) - datetime.timedelta(hours=2))
     quotes.index = quotes.Time
 
     print(quotes)
-    # select desired range of dates
-    mask = (quotes.index > start_date) & (quotes.index <= end_date)
-    quotes = quotes.loc[mask]
 
-    print(quotes)
+
 
     fig, ax = plt.subplots()
     ax.plot(quotes['Close'])
     ax.set_title('Black Regions')
-    anormalies = pd.read_csv("results/EURUSD/all_anomalies.csv")
+    anormalies = pd.read_csv("results/"+currency+"/all_anomalies.csv")
 
     anormalies['Time'] = anormalies['DateHour'].apply(lambda x: pd.to_datetime(x))
     anormalies.index = anormalies.Time
-    mask = (anormalies.index > start_date) & (anormalies.index <= end_date)
-    anormalies = anormalies.loc[mask]
+    mask_2 = (anormalies.index >= start_date) & (anormalies.index <= end_date)
+    anormalies = anormalies.loc[mask_2]
+    print("anormalies")
+    print(anormalies)
     #xdate = [datetime.datetime.fromtimestamp(i) for i in quotes['Time']]
 
     for index, row in anormalies.iterrows():
